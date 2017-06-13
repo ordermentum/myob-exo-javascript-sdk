5a032ccb1c05c2d60974c2f20c6f9d9ce95a697c
Michael Cooper <mikeyjc@gmail.com>
2017-06-13 15:59:06 +1000

    add stock to the exo resources

--- src/resources/index.js
+++ src/resources/index.js
@@ -3,5 +3,6 @@ import resource from './resource';
 export default {
   salesorder: resource('/salesorder'),
   stockitem: resource('/stockitem'),
+  stock: resource('/stock'),
   token: resource('/token'),
 };
